8e63297268174ef9f5de4176927a5f4ff29f81bb
Onur <43381986+onursert@users.noreply.github.com>
2019-12-14 16:39:51 +0300

    Add files via upload

---
+++ DNSChanger/DNSChanger.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DNSChanger
+{
+    partial class DNSChanger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDNS1 = new System.Windows.Forms.TextBox();
+            this.textBoxDNS2 = new System.Windows.Forms.TextBox();
+            this.buttonSetDNS = new System.Windows.Forms.Button();
+            this.buttonDefault = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select DNS: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type DNS: ";
+            // 
+            // textBoxDNS1
+            // 
+            this.textBoxDNS1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDNS1.Location = new System.Drawing.Point(87, 60);
+            this.textBoxDNS1.Name = "textBoxDNS1";
+            this.textBoxDNS1.Size = new System.Drawing.Size(300, 20);
+            this.textBoxDNS1.TabIndex = 3;
+            // 
+            // textBoxDNS2
+            // 
+            this.textBoxDNS2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDNS2.Location = new System.Drawing.Point(87, 86);
+            this.textBoxDNS2.Name = "textBoxDNS2";
+            this.textBoxDNS2.Size = new System.Drawing.Size(300, 20);
+            this.textBoxDNS2.TabIndex = 4;
+            // 
+            // buttonSetDNS
+            // 
+            this.buttonSetDNS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSetDNS.Location = new System.Drawing.Point(312, 112);
+            this.buttonSetDNS.Name = "buttonSetDNS";
+            this.buttonSetDNS.Size = new System.Drawing.Size(75, 23);
+            this.buttonSetDNS.TabIndex = 5;
+            this.buttonSetDNS.Text = "Set DNS";
+            this.buttonSetDNS.UseVisualStyleBackColor = true;
+            this.buttonSetDNS.Click += new System.EventHandler(this.buttonSetDNS_Click);
+            // 
+            // buttonDefault
+            // 
+            this.buttonDefault.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDefault.Location = new System.Drawing.Point(231, 112);
+            this.buttonDefault.Name = "buttonDefault";
+            this.buttonDefault.Size = new System.Drawing.Size(75, 23);
+            this.buttonDefault.TabIndex = 6;
+            this.buttonDefault.Text = "Default";
+            this.buttonDefault.UseVisualStyleBackColor = true;
+            this.buttonDefault.Click += new System.EventHandler(this.buttonDefault_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "github.com/onursert";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 160);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonDefault);
+            this.Controls.Add(this.buttonSetDNS);
+            this.Controls.Add(this.textBoxDNS2);
+            this.Controls.Add(this.textBoxDNS1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DNS Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDNS1;
+        private System.Windows.Forms.TextBox textBoxDNS2;
+        private System.Windows.Forms.Button buttonSetDNS;
+        private System.Windows.Forms.Button buttonDefault;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DNSChanger/DNSChanger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management;
+
+namespace DNSChanger
+{
+    public partial class DNSChanger : Form
+    {
+        public DNSChanger()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Google DNS");
+            comboBox1.Items.Add("Open DNS");
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                textBoxDNS1.Text = "8.8.8.8";
+                textBoxDNS2.Text = "8.8.4.4";
+            }
+
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                textBoxDNS1.Text = "208.67.222.222";
+                textBoxDNS2.Text = "208.67.220.220";
+            }
+        }
+
+        int setORdefault; //default = 0 , set = 1
+
+        private void buttonDefault_Click(object sender, EventArgs e)
+        {
+            setORdefault = 0;
+            setDNS("", "");
+        }
+
+        private void buttonSetDNS_Click(object sender, EventArgs e)
+        {
+            setORdefault = 1;
+            setDNS(textBoxDNS1.Text, textBoxDNS2.Text);
+        }
+
+        public void setDNS(string dns1, string dns2)
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                if ((bool)mo["IPEnabled"])
+                {
+                    ManagementBaseObject DnsEntry = mo.GetMethodParameters("SetDNSServerSearchOrder");
+
+                    if (setORdefault == 1)
+                    {
+                        string[] dns = new string[2];
+                        dns[0] = dns1;
+                        dns[1] = dns2;
+                        DnsEntry["DNSServerSearchOrder"] = dns;
+                    }
+                    else
+                    {
+                        DnsEntry["DNSServerSearchOrder"] = new string[0];
+                    }
+
+                    ManagementBaseObject DnsMbo = mo.InvokeMethod("SetDNSServerSearchOrder", DnsEntry, null);
+
+                    int returnCode = int.Parse(DnsMbo["returnvalue"].ToString());
+                    if (returnCode == 0)
+                    {
+                        MessageBox.Show("Successful.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("You need to run this app with administrator privileges.");
+                    }
+                }
+            }
+        }
+    }
+}
